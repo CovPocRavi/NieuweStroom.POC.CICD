760f445797377a2c93dabd9d75b4207b5bce44cf
Ravi Harnam <r.harnam@covadis.nl>
2019-04-01 14:36:07 +0200

    Added Invoice Model Added NieuweStroomContext Removed consolewriteline from main in Program

---
+++ NieuweStroom.POC.CICD/Models/Invoice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace NieuweStroom.POC.CICD.Models
+{
+    public class Invoice
+    {
+        public Guid Id { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public Guid? MeterId { get; set; }
+
+        public int InvoiceNumber { get; set; }
+
+        public DateTimeOffset? InvoiceDate { get; set; }
+
+        public DateTimeOffset IntervalStart { get; set; }
+
+        public DateTimeOffset IntervalEnd { get; set; }
+
+        public double AmountExcVat { get; set; }
+
+        public double AmountVat { get; set; }
+
+        public double AmountIncVat { get; set; }
+
+        public string Description { get; set; }
+
+        public int InvoiceTypeId { get; set; }
+
+        public int PublicUtilityId { get; set; }
+
+        public Guid? DocumentId { get; set; }
+    }
+}
