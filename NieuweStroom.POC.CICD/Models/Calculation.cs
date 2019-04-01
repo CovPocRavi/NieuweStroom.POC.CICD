909ed5b385f2bd14422ab954646b5fcebec9c76f
Ravi Harnam <r.harnam@covadis.nl>
2019-04-01 20:17:43 +0200

    added calculation class

---
+++ NieuweStroom.POC.CICD/Models/Calculation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NieuweStroom.POC.CICD.Models
+{
+    public class Calculation
+    {
+    }
+}
