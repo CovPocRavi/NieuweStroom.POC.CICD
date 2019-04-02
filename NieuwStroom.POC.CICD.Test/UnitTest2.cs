8e9c850285509cca5b13e933cbc8f0a332a7635c
Ravi Harnam <r.harnam@covadis.nl>
2019-04-02 11:11:30 +0200

    added extra unittest

---
+++ NieuwStroom.POC.CICD.Test/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NieuwStroom.POC.CICD.Test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //arrange
+            var priceCalculator = new NieuweStroom.POC.CICD.PriceCalculator();
+
+            // act
+            for (int i = 0; i < 1000; i++)
+            {
+                int actual = priceCalculator.CalculatePrice(i, i + 1);
+                int expected = i * (i + 1);
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+        }
+    }
+}
