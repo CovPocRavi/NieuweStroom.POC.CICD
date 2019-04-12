9a68229f66051108cbc873543e0e0850887ed0c9
Ravi Harnam <r.harnam@covadis.nl>
2019-04-12 08:50:21 +0200

    Database koppeling en migratie van de database doorgevoerd

--- NieuweStroom.POC.CICD/Context/NieuweStroomContext.cs
+++ NieuweStroom.POC.CICD/Context/NieuweStroomContext.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using NieuweStroom.POC.CICD.Models;
 
 namespace NieuweStroom.POC.CICD.Context
 {
     public class NieuweStroomContext : DbContext
     {
-        //hier komt de context ldf;g fdgl dfg
+        public NieuweStroomContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Invoice> Invoices { get; set; }
     }
 }
--- NieuweStroom.POC.CICD/Startup.cs
+++ NieuweStroom.POC.CICD/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using NieuweStroom.POC.CICD.Context;
 
 namespace NieuweStroom.POC.CICD
 {
@@ -32,7 +33,7 @@ namespace NieuweStroom.POC.CICD
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<NieuweStroomContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:NieuweStroomPOC"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
