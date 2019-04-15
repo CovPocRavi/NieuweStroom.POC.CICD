1268734008205aabb05b7a4e0978ec485f30d16e
Ravi Harnam <r.harnam@covadis.nl>
2019-04-15 10:13:44 +0200

    Added V2 PoC to upgrade PoC to asp.netcore 2.2

---
+++ NieuwStroom.POC.CICD.Test/BasicIntegrationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+
+namespace NieuwStroom.POC.CICD.Test
+{
+    public class BasicIntegrationTest 
+    {
+
+    }
+}
---
+++ NieuweStroom.POC.CICD.V2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NieuweStroom.POC.CICD.V2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
