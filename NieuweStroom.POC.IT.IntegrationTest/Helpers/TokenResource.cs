6003d9fb8cfabb5ad27f371d4c414ce00a8093e4
Ravi Harnam <r.harnam@covadis.nl>
2019-05-01 15:28:26 +0200

    test pkg

--- NieuweStroom.POC.IT.IntegrationTest/Helpers/TokenResource.cs
+++ NieuweStroom.POC.IT.IntegrationTest/Helpers/TokenResource.cs
@@ -2,6 +2,6 @@ namespace NieuweStroom.POC.IT.IntegrationTest.Helpers
 {
     public class TokenResource
     {
-        public string Token { get; set; }
+        public string Token { get; set; } //test
     }
 }
