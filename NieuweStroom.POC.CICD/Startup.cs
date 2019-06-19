276df7733518479069e5eb4fc556fedbc72d6fe7
Ravi Harnam <r.harnam@covadis.nl>
2019-06-19 10:40:00 +0200

    TestMergeFromFeatureBrancheToDevelopmentBranche

--- NieuweStroom.POC.CICD/Startup.cs
+++ NieuweStroom.POC.CICD/Startup.cs
@@ -36,7 +36,7 @@ namespace NieuweStroom.POC.CICD
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        //Testing merge from FeatureBranche to Development (GitFlow)
+        //Testing merge from FeatureBranche to Development (GitFlow).
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
